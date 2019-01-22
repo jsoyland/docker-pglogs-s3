05de7c6a25650da8280b2832be4d1c3519005fb9
Luke Woodward <luke@tozny.com>
2019-01-22 13:52:10 -0800

    Fix header check and ctrl-c handling

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ var cronSchedule = process.env.CRON_SCHEDULE.replace(/["']+/g, '')
 
 console.log("Starting backup service on cron schedule: " + cronSchedule)
 
-new CronJob(cronSchedule, function(){
+const job = new CronJob(cronSchedule, function(){
   console.log('Running cron task at ' + new Date())
   // run the backup script
   handler(null, null, function(){})
-}, null, true, 'America/Los_Angeles');
+}, null, true, 'America/Los_Angeles')
+
+process.on('SIGINT', function(){
+  job.stop()
+  process.exit(0)
+})
 
 // run without event or context for execution outside of lambda environment
 //handler(null,null,function(){})
